38711ad94070bcf256657f76a64712794d6f0823
Sergio Mover <sergio@fbk.eu>
2016-04-18 10:31:10 -0600

    Created a stub project

---
+++ src/main/java/edu/colorado/plv/fixr/Main.java
@@ -0,0 +1,10 @@
+package edu.colorado.plv.fixr;
+
+
+public class Main {
+
+  public static void main(String[] args) {
+     System.out.println("test");
+  }
+  
+}
---
+++ src/test/java/edu/colorado/plv/fixr/DummyTest.java
@@ -0,0 +1,11 @@
+package edu.colorado.plv.fixr;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class DummyTest {
+    @Test
+    public void dummyTest() {
+        assertEquals(0, 0);
+    }
+}
