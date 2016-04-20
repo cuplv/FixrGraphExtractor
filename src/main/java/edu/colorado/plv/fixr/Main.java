a50c9b670a4f67389e007435f8eb720a1951edc4
smover <sergio.mover@gmail.com>
2016-04-19 23:56:23 -0600

    Still test code - initialization of soot and printing of CFGs

--- src/main/java/edu/colorado/plv/fixr/Main.java
+++ src/main/java/edu/colorado/plv/fixr/Main.java
@@ -1,8 +1,23 @@
 package edu.colorado.plv.fixr;
 
+import java.util.Iterator;
+import java.util.Map;
+
 import soot.Body;
+import soot.BodyTransformer;
+import soot.PackManager;
+import soot.PhaseOptions;
+import soot.Scene;
+import soot.SootClass;
+import soot.Transform;
+import soot.Unit;
+import soot.jimple.JimpleBody;
+import soot.options.Options;
 import soot.toolkits.graph.BriefUnitGraph;
 import soot.toolkits.graph.DirectedGraph;
+import soot.toolkits.graph.ExceptionalBlockGraph;
+import soot.toolkits.graph.Block;
+import soot.toolkits.graph.BriefBlockGraph;;
 
 public class Main {
 	
@@ -18,12 +33,25 @@ public class Main {
   	
   	System.out.println(classPath);
   	SootHelper.configure(classPath);
-  	
+
+  	Scene.v().addBasicClass(className, SootClass.HIERARCHY);
+  	Scene.v().loadNecessaryClasses();
+
   	Body b = SootHelper.getMethodBody(className, methodName);
-  	DirectedGraph g = new BriefUnitGraph(b);  	
-  	SootHelper.dumpToDot(g, b, "test.dot");  	
+
+  	if (b instanceof JimpleBody) {
+  		System.out.println(b.toString());
+  	}
   	
+  	BriefUnitGraph g = new BriefUnitGraph(b);  	
+  	SootHelper.dumpToDot(g, b, "brief_unit_graph.dot");
+  	
+  	BriefBlockGraph g2 = new BriefBlockGraph(b);  	
+  	SootHelper.dumpToDot(g2, b, "brief_block_graph.dot");
+  	
+  	ExceptionalBlockGraph g3 = new ExceptionalBlockGraph(b);   	
+  	SootHelper.dumpToDot(g3, b, "exceptional_block_graph.dot");  	  	 	 
+
   	System.out.println("Done");
   }
-  
 }
--- src/main/java/edu/colorado/plv/fixr/SootHelper.java
+++ src/main/java/edu/colorado/plv/fixr/SootHelper.java
@@ -5,6 +5,7 @@ import soot.PhaseOptions;
 import soot.Scene;
 import soot.SootClass;
 import soot.SootMethod;
+import soot.jimple.JimpleBody;
 import soot.options.Options;
 
 import soot.toolkits.graph.DirectedGraph;
@@ -16,37 +17,73 @@ public class SootHelper {
 		Options.v().set_verbose(false);
 		Options.v().set_keep_line_number(true);
 		Options.v().set_src_prec(Options.src_prec_class);
-		Options.v().set_soot_classpath(classpath);
-		Options.v().set_prepend_classpath(true);
-
-		//jj.uce
+		//Options.v().set_soot_classpath("/usr/lib/jvm/jdk1.7.0/jre/lib/rt.jar:/home/sergio/works/projects/muse/repos/FixrGraphExtractor/src/test/resources");
+		Options.v().set_soot_classpath(classpath);		
+		Options.v().set_prepend_classpath(true);	
 		
-		PhaseOptions.v().setPhaseOption("jb", "off");		
-		PhaseOptions.v().setPhaseOption("bb", "off");
-		PhaseOptions.v().setPhaseOption("wjpp", "off");
-		PhaseOptions.v().setPhaseOption("wspp", "off");
-		PhaseOptions.v().setPhaseOption("cg", "off");
-		PhaseOptions.v().setPhaseOption("wstp", "off");
-		PhaseOptions.v().setPhaseOption("wsop", "off");
-		PhaseOptions.v().setPhaseOption("wjtp", "off");
-		PhaseOptions.v().setPhaseOption("wjop", "off");
-		PhaseOptions.v().setPhaseOption("wjap", "off");
-		PhaseOptions.v().setPhaseOption("shimple", "off");
-		PhaseOptions.v().setPhaseOption("stp", "off");
-		PhaseOptions.v().setPhaseOption("sop", "off");
-		PhaseOptions.v().setPhaseOption("jtp", "off");
-		PhaseOptions.v().setPhaseOption("jop", "off");
-		PhaseOptions.v().setPhaseOption("jap", "off");
-		PhaseOptions.v().setPhaseOption("gb", "off");
-		PhaseOptions.v().setPhaseOption("gop", "off");
-		PhaseOptions.v().setPhaseOption("bb", "off");
-		PhaseOptions.v().setPhaseOption("bop", "off");
-		PhaseOptions.v().setPhaseOption("tag", "off");
-		PhaseOptions.v().setPhaseOption("db", "off");
+		PhaseOptions.v().setPhaseOption("jb", "enabled:false");
+		/* We want to parse the code from source
+		 * Phase
+     * jj Creates a JimpleBody for each method directly from source
+     *
+     * Subphases
+     * jj.ls        Local splitter: one local per DU-UD web 
+     * jj.a        Aggregator: removes some unnecessary copies 
+     * jj.ule      Unused local eliminator 
+     * jj.tr       Assigns types to locals 
+     * jj.ulp      Local packer: minimizes number of locals 
+     * jj.lns      Local name standardizer 
+     * jj.cp       Copy propagator 
+     * jj.dae      Dead assignment eliminator
+     * jj.cp-ule   Post-copy propagation unused local eliminator 
+     * jj.lp       Local packer: minimizes number of locals 
+     * jj.ne       Nop eliminator 
+     * jj.uce      Unreachable code eliminator  
+     */		
+		PhaseOptions.v().setPhaseOption("jj", "use-original-names:true");
+		//PhaseOptions.v().setPhaseOption("jj", "enabled:true");
+		PhaseOptions.v().setPhaseOption("jj.ls", "enabled:false");
+		PhaseOptions.v().setPhaseOption("jj.a", "enabled:true");
+		PhaseOptions.v().setPhaseOption("jj.ule", "enabled:true");
+		PhaseOptions.v().setPhaseOption("jj.tr", "enabled:true");
+		PhaseOptions.v().setPhaseOption("jj.ulp", "enabled:true");
+		PhaseOptions.v().setPhaseOption("jj.lns", "enabled:true");
+		PhaseOptions.v().setPhaseOption("jj.cp", "enabled:true");
+		PhaseOptions.v().setPhaseOption("jj.dae", "enabled:true");
+		PhaseOptions.v().setPhaseOption("jj.cp-ule", "enabled:true");
+		PhaseOptions.v().setPhaseOption("jj.lp", "enabled:true");
+		PhaseOptions.v().setPhaseOption("jj.ne", "enabled:true");
+		PhaseOptions.v().setPhaseOption("jj.uce", "enabled:true");
 		
-		PhaseOptions.v().setPhaseOption("tag.ln", "on");
-		PhaseOptions.v().setPhaseOption("jj.uce", "on");
-
+		PhaseOptions.v().setPhaseOption("cg", "enabled:false");
+		PhaseOptions.v().setPhaseOption("wjtp", "enabled:false");
+		PhaseOptions.v().setPhaseOption("wjop", "enabled:false");
+		PhaseOptions.v().setPhaseOption("wjap", "enabled:false");
+		
+		PhaseOptions.v().setPhaseOption("jtp", "enabled:true");
+		PhaseOptions.v().setPhaseOption("jop", "enabled:false");
+		PhaseOptions.v().setPhaseOption("jap", "enabled:false");
+		
+		PhaseOptions.v().setPhaseOption("bb", "enabled:false");
+		
+		PhaseOptions.v().setPhaseOption("wjpp", "enabled:false");
+		PhaseOptions.v().setPhaseOption("wspp", "enabled:false");
+		PhaseOptions.v().setPhaseOption("wstp", "enabled:false");
+		PhaseOptions.v().setPhaseOption("wsop", "enabled:false");
+		
+		
+		PhaseOptions.v().setPhaseOption("shimple", "enabled:false");
+		PhaseOptions.v().setPhaseOption("stp", "enabled:false");
+		PhaseOptions.v().setPhaseOption("sop", "enabled:false");		
+		
+		
+		PhaseOptions.v().setPhaseOption("gb", "enabled:false");
+		PhaseOptions.v().setPhaseOption("gop", "enabled:false");
+		PhaseOptions.v().setPhaseOption("bb", "enabled:false");
+		PhaseOptions.v().setPhaseOption("bop", "enabled:false");
+		PhaseOptions.v().setPhaseOption("tag", "enabled:false");
+		PhaseOptions.v().setPhaseOption("db", "enabled:false");
+		
 		Options.v().set_whole_program(true);
 	}	
 	
@@ -54,7 +91,7 @@ public class SootHelper {
 		SootClass c = Scene.v().loadClassAndSupport(className);
   	c.setApplicationClass();
   	SootMethod m = c.getMethodByName(methodName);
-  	Body b = m.retrieveActiveBody();
+  	Body b = m.retrieveActiveBody();  	
   	return b;
 	}
 	
@@ -63,6 +100,5 @@ public class SootHelper {
   	DotGraph viewgraph = gr.drawCFG(g,b); 	   
   	
   	viewgraph.plot(fileName);
-	}
-	
+	}	
 }
