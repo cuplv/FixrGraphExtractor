1a1db9418cf8108e85dcc56b773729f0ba46a830
smover <sergio.mover@gmail.com>
2016-04-24 10:50:15 -0600

    updated test case for slicing

--- src/test/resources/slice/TestSlice.java
+++ src/test/resources/slice/TestSlice.java
@@ -3,18 +3,55 @@ package slice;
 import java.lang.Math.*;
 
 public class TestSlice {
-  public int m1() {
+  public void m1() {
+    int a = 0;
+
+    java.lang.Math.abs(a);
+  }
+  
+  public void m2() {
     // Slice a sequence - get rid of b blocks
 
     int a;
-    int b;    
+    int b;
 
-    a = 0; // {}
+    a = 0; // {a}
     b = 0; // {}
     
     java.lang.Math.abs(a); // {a}
-
-    return a;
   }
-  
+
+  public void m3() {
+    // Slice a sequence - get rid of b blocks
+
+    int a;
+    int b;
+    a = 0;
+    b = 0;
+    a = b;    
+    java.lang.Math.abs(a); // {a,b}
+  }
+
+    public void m4() {
+    // Slice a sequence - get rid of b blocks
+
+    int a;
+    int b;
+    int c;
+
+    a = 0;
+    b = 0;
+    c = 0;
+
+    if (b == 0) {
+      a = a + b;
+    }
+    else {
+      c = a;
+    }
+
+    a = b;
+    java.lang.Math.abs(a); // {a}
+  }
+
 }
