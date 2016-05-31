357c5d758edcc68bfde7a32e3acec739dff4959b
Rhys Braginton Pettee Olsen <rhol9958@colorado.edu>
2016-05-31 15:16:54 -0600

    Extend protobuf conversion

--- src/main/scala/edu/colorado/plv/fixr/abstraction/AcdfgToProtobuf.scala
+++ src/main/scala/edu/colorado/plv/fixr/abstraction/AcdfgToProtobuf.scala
@@ -3,8 +3,61 @@ package edu.colorado.plv.fixr.abstraction
 import edu.colorado.plv.fixr.protobuf.ProtoAcdfg
 
 /**
- * Created by rhys on 5/31/16.
+ * AcdfgToProtobuf
+ *   Class implementing conversion from abstract control data flow graph (ACDFG)
+ *   to Protobuf format.
+ *
+ *   @author Rhys Braginton Pettee Olsen <rhol9958@colorado.edu>
+ *   @group  University of Colorado at Boulder CUPLV
  */
-class AcdfgToProtobuf {
-  var protobuf : ProtoAcdfg.Acdfg.Builder = ProtoAcdfg.Acdfg.newBuilder()
+
+class AcdfgToProtobuf(acdfg : Acdfg) {
+  var builder : ProtoAcdfg.Acdfg.Builder = ProtoAcdfg.Acdfg.newBuilder()
+  acdfg.edges.foreach {
+    case (id : Long, edge : this.acdfg.ControlEdge) =>
+      val protoControlEdge: ProtoAcdfg.Acdfg.ControlEdge.Builder =
+        ProtoAcdfg.Acdfg.ControlEdge.newBuilder()
+      protoControlEdge.setId(id)
+      protoControlEdge.setFrom(edge.from)
+      protoControlEdge.setTo(edge.to)
+      builder.addControlEdge(protoControlEdge)
+    case (id : Long, edge : this.acdfg.DefEdge) =>
+      val protoDefEdge: ProtoAcdfg.Acdfg.DefEdge.Builder =
+        ProtoAcdfg.Acdfg.DefEdge.newBuilder()
+      protoDefEdge.setId(id)
+      protoDefEdge.setFrom(edge.from)
+      protoDefEdge.setTo(edge.to)
+      builder.addDefEdge(protoDefEdge)
+    case (id : Long, edge : this.acdfg.UseEdge) =>
+      val protoUseEdge: ProtoAcdfg.Acdfg.UseEdge.Builder =
+        ProtoAcdfg.Acdfg.UseEdge.newBuilder()
+      protoUseEdge.setId(id)
+      protoUseEdge.setFrom(edge.from)
+      protoUseEdge.setTo(edge.to)
+      builder.addUseEdge(protoUseEdge)
+  }
+  acdfg.nodes.foreach {
+    case (id : Long, node : this.acdfg.DataNode) =>
+      val protoDataNode : ProtoAcdfg.Acdfg.DataNode.Builder =
+        ProtoAcdfg.Acdfg.DataNode.newBuilder()
+      protoDataNode.setId(id)
+      protoDataNode.setName(node.name)
+      protoDataNode.setType(node.datatype)
+      builder.addDataNode(protoDataNode)
+    case (id : Long, node : this.acdfg.MiscNode) =>
+      val protoMiscNode : ProtoAcdfg.Acdfg.MiscNode.Builder =
+        ProtoAcdfg.Acdfg.MiscNode.newBuilder()
+      protoMiscNode.setId(id)
+      builder.addMiscNode(protoMiscNode)
+    case (id : Long, node : this.acdfg.MethodNode) =>
+      val protoMethodNode : ProtoAcdfg.Acdfg.MethodNode.Builder =
+        ProtoAcdfg.Acdfg.MethodNode.newBuilder()
+      protoMethodNode.setId(id)
+      protoMethodNode.s
+      node.assignee
+      node.id
+      node.arguments
+      builder.addMiscNode(protoMiscNode)
+  }
+
 }
