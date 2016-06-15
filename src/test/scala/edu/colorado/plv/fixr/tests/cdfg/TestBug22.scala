e7b5f3576f4d55d00fab407c811c0999199ba12e
smover <sergio.mover@gmail.com>
2016-06-15 17:41:46 -0600

    Add soot reset before each test

--- src/test/scala/edu/colorado/plv/fixr/tests/cdfg/TestBug22.scala
+++ src/test/scala/edu/colorado/plv/fixr/tests/cdfg/TestBug22.scala
@@ -2,16 +2,18 @@ package edu.colorado.plv.fixr.tests.cdfg
 
 import edu.colorado.plv.fixr.extractors._;
 import org.scalatest.FunSuite
+import org.scalatest.BeforeAndAfter
+import edu.colorado.plv.fixr.SootHelper
 
 /**
  * Regression tests for bug 22
  */
-class TestBug22 extends FunSuite {
+class TestBug22 extends FunSuite with BeforeAndAfter {
   
    
   def testExtraction(className : String, classPath : String) = 
   {
-  		val options : ExtractorOptions = new ExtractorOptions();
+    val options : ExtractorOptions = new ExtractorOptions();
       options.className = className;
       options.methodName = null;
       options.readFromSources = true;
@@ -24,9 +26,13 @@ class TestBug22 extends FunSuite {
       var extractor : Extractor = new MultipleExtractor(options);
       extractor.extract() 
   }   
-   
-   test("abstract_method") {testExtraction("bugs.Bug_022",
-       "./src/test/resources/javasources")}
-   test("interface") {testExtraction("bugs.Bug_022_02",
-       "./src/test/resources/javasources")}
+
+  before {
+    SootHelper.reset();
+  }
+  
+  test("abstract_method") {testExtraction("bugs.Bug_022",
+    "./src/test/resources/javasources")}
+  test("interface") {testExtraction("bugs.Bug_022_02",
+    "./src/test/resources/javasources")}
 }
